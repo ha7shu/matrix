71da951e9d87c2062b885030844de408a1700de3
ha7shu <harshusharma9896@gmail.com>
2023-04-14 14:38:23 +0530

    refactored code

--- main.c
+++ main.c
@@ -3,105 +3,104 @@
 
 int main()
 {
-    printf("\nThis is a program to perform different operations on matrices. Most common operations are defined in \"Matrix.h\" header file.\n\n");
-    int rows, cols;
+    printf("\nThis is a program to perform different operations on matrices.\nFunction for various operations are defined in \"Matrix.h\" header file.\n\nStart by entering the number of rows and columns of the matrix in following prompts.\n\n");
+    int rows1, cols1;
     printf("Enter the number of rows: ");
-    scanf("%d", &rows);
+    scanf("%d", &rows1);
     printf("Enter the number of columns: ");
-    scanf("%d", &cols);
-    printf("\nOrder of the matrix == %d X %d \n", rows, cols);
-    int matrix[rows][cols];
-    printf("\nEnter the elements of the matrix: \n");
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++){
+    scanf("%d", &cols1);
+    printf("\nOrder of the Matrix == %d X %d \n", rows1, cols1);
+    int matrix1[rows1][cols1];
+    printf("\nEnter the elements of the Matrix : \n");
+    for (int i = 0; i < rows1; i++){
+        for (int j = 0; j < cols1; j++){
             printf("Element %d%d: ", i+1, j+1);
-            scanf("%d", &matrix[i][j]);
+            scanf("%d", &matrix1[i][j]);
         }
     }
-    printf("\nThe matrix formed is: \n");
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
-            printf("%d ", matrix[i][j]);
+    printf("\nThe Matrix formed is: \n");
+    for(int i = 0; i < rows1; i++){
+        for(int j = 0; j < cols1; j++){
+            printf("%d ", matrix1[i][j]);
         }
         printf("\n");
     }
-    int transPose[cols][rows];
-        printf("\nSum of all elements in the matrix == %d \n", sumOfElements(rows, cols, matrix));
-        transpose(rows, cols, matrix, transPose);
-        printf("\nTranspose of the matrix is: \n");
-        for(int i = 0; i < cols; i++){
-            for(int j = 0; j < rows; j++){
+    int transPose[cols1][rows1];
+        printf("\nSum of all elements in the Matrix == %d \n", sumOfElements(rows1, cols1, matrix1));
+        transpose(rows1, cols1, matrix1, transPose);
+        printf("\nTranspose of the Matrix is: \n");
+        for(int i = 0; i < cols1; i++){
+            for(int j = 0; j < rows1; j++){
                 printf("%d ", transPose[i][j]);
             }
             printf("\n");
         }
-    if(rows == cols){    
-        printf("\nDeterminant of the matrix == %d \n", determinant(rows, matrix));
+    if(rows1 == cols1){    
+        printf("\nDeterminant of the Matrix == %d \n", determinant(rows1, matrix1));
     } else {
-        printf("\nMatrix is not a square matrix and its determinant is not possible.\n");
+        printf("\nThis Matrix %d X %d is not a square matrix and its determinant is not possible.\n", rows1, cols1);
     }    
-    if(rows == cols){
-        int Diag[rows], trace = 0;
-        diagonal(rows, matrix, Diag);
-        printf("\nDiagonal of the matrix is: ");
-        for(int i = 0; i < rows; i++){
+    if(rows1 == cols1){
+        int Diag[rows1], trace = 0;
+        diagonal(rows1, matrix1, Diag);
+        printf("\nDiagonal of the Matrix is: ");
+        for(int i = 0; i < rows1; i++){
             printf("%d ", Diag[i]);
             trace += Diag[i];
         } 
-        printf("\n\nSum of diagnonal elements i.e trace of matrix is == %d \n", trace);
+        printf("\n\nSum of diagnonal elements i.e trace of Matrix is == %d \n", trace);
     }
     char choice;
     printf("\nDo you want to perform Addition and Multiplicaton of two matrices? (y/n): ");
     scanf(" %c", &choice);
     if(choice == 'y' || choice == 'Y'){
-        int rows1, cols1;
-        printf("\nEnter the number of rows for 2nd matrix : ");
-        scanf("%d", &rows1);
-        printf("\nEnter the number of columns for 2nd matrix : ");
-        scanf("%d", &cols1);
-        int matrix1[rows1][cols1];
-        printf("\n\nEnter the elements of the matrix: \n");
-        for (int i = 0; i < rows1; i++){
-            for (int j = 0; j < cols1; j++){
+        int rows2, cols2;
+        printf("\nEnter the number of rows for 2nd Matrix : ");
+        scanf("%d", &rows2);
+        printf("Enter the number of columns for 2nd Matrix : ");
+        scanf("%d", &cols2);
+        int matrix2[rows2][cols2];
+        printf("\n\nEnter the elements of the Matrix: \n");
+        for (int i = 0; i < rows2; i++){
+            for (int j = 0; j < cols2; j++){
                 printf("Element %d%d: ", i+1, j+1);
-                scanf("%d", &matrix1[i][j]);
+                scanf("%d", &matrix2[i][j]);
             }
         }
         printf("\nSecond Matrix formed is : \n");
-        for(int i = 0; i < rows1; i++){
-            for(int j = 0; j < cols1; j++){
-                printf("%d ", matrix1[i][j]);
+        for(int i = 0; i < rows2; i++){
+            for(int j = 0; j < cols2; j++){
+                printf("%d ", matrix2[i][j]);
             }
             printf("\n");
         }
-        if(rows == rows1 && cols == cols1){
-        int result1[rows1][cols1];
-        addTwoMatrices(rows1, cols1, matrix, matrix1, result1);
-        printf("\nSum of 1st matrix and 2nd matrix is : \n");
-        for(int i = 0; i < rows1; i++){
-            for(int j = 0; j < cols1; j++){
-                printf("%d ", result1[i][j]);
-            }
-            printf("\n");
-        }
-        } else {
-            printf("\nSum of these two matrices is not possible because either \nnumber of rows of matrix 1 (%d) != number of rows of matrix 2 (%d) \nor \nnumber of columns of matrix 1 (%d) != numeber of columns of matrix 2 (%d) \n", rows, rows1, cols, cols1);
-        }
-        if(cols == rows1){
-        int result2[rows1][cols1];
-        multiplyTwoMatrices(rows, cols, matrix, rows1, cols1, matrix1, result2);
-        printf("\nProduct of 1st matrix and 2nd matrix is : \n");
-        for(int i = 0; i < rows1; i++){
-            for(int j = 0; j < cols1; j++){
-                printf("%d ", result2[i][j]);
+        if(rows1 == rows2 && cols1 == cols2){
+        int result[rows2][cols2];
+        addTwoMatrices(rows2, cols2, matrix1, matrix2, result);
+        printf("\nSum of Matrix 1 and Matrix 2 is : \n");
+        for(int i = 0; i < rows2; i++){
+            for(int j = 0; j < cols2; j++){
+                printf("%d ", result[i][j]);
             }
             printf("\n");
         }
         } else {
-            printf("\nProduct of these two matrices is not possible because \nnumber of columns of matrix 1 %d != number of rows of matrix 2 %d \n", cols, rows1);
+            printf("\nSum of these two matrices is not possible because either \nnumber of rows of Matrix 1 (%d) != number of rows of Matrix 2 (%d) \nor \nnumber of columns of Matrix 1 (%d) != numeber of columns of Matrix 2 (%d) \n", rows1, rows2, cols1, cols2);
         }
-    } else {
-        printf("\nThank you for using this program.\n");
+        if(cols1 == rows2){
+        int multiplyResult[rows2][cols2];
+        multiplyTwoMatrices(rows1, cols1, matrix1, rows2, cols2, matrix2, multiplyResult);
+        printf("\nProduct of Matrix 1 and Matrix 2 is : \n");
+        for(int i = 0; i < rows2; i++){
+            for(int j = 0; j < cols2; j++){
+                printf("%d ", multiplyResult[i][j]);
+            }
+            printf("\n");
+        }
+        } else {
+            printf("\nProduct of these two matrices is not possible because \nnumber of columns of Matrix 1 %d != number of rows of Matrix 2 %d \n", cols1, rows2);
+        }
     }
+    printf("\nThank you for using this program.\n\n");
     return 0;
 }
